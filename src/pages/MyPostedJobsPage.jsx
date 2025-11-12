95fef0737817b80058609c4eb01b177d1728833d
ratul43 <kmratul971@gmail.com>
2025-11-12 23:14:43 +0600

    my posted job page responsive for all devices

--- src/pages/MyPostedJobsPage.jsx
+++ src/pages/MyPostedJobsPage.jsx
@@ -90,7 +90,7 @@ const MyPostedJobsPage = () => {
                   Contact: {job.userEmail}
                 </p>
               </div>
-              <div className="flex gap-2">
+              <div className="flex md:flex-row flex-col gap-2">
                 
                 <Link to={`/updateJob/${job._id}`} className="bg-cyan-500 text-white px-4 py-2 rounded hover:bg-red-600">
                   Update
